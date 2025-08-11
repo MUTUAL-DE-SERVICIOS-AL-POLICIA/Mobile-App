ec0b668b372d4781d7c35cf3222560ee1e47807d
napoleonbaley2011 <leonellima56@gmail.com>
2025-08-11 10:08:53 -0400

    Se creo el espacio dentro de prestamos para colocar los widgets de Calculadora de prestamos

--- lib/screens/pages/loans_pages/loans_page_new.dart
+++ lib/screens/pages/loans_pages/loans_page_new.dart
@@ -2,6 +2,7 @@ import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:muserpol_pvt/bloc/loan/loan_bloc.dart';
 import 'package:muserpol_pvt/model/biometric_user_model.dart';
 import 'package:muserpol_pvt/model/loan_model.dart';
@@ -27,88 +28,107 @@ class _ScreenLoansNewState extends State<ScreenLoansNew> {
   @override
   Widget build(BuildContext context) {
     final loanBloc = BlocProvider.of<LoanBloc>(context, listen: true);
-    return RefreshIndicator(
-      onRefresh: () async {
-        reloadLoans();
-      },
-      child: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            children: [
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Mis Prestamos :',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Theme.of(context).brightness == Brightness.dark
-                        ? Colors.white
-                        : Colors.black,
-                    fontSize: 18.sp,
+    return Scaffold(
+      body: RefreshIndicator(
+        onRefresh: () async {
+          reloadLoans();
+        },
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Mis Prestamos :',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).brightness == Brightness.dark
+                          ? Colors.white
+                          : Colors.black,
+                      fontSize: 18.sp,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 20.h),
-              loanBloc.state.existLoan
-                  ? Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        if (loanBloc.state.loan!.notification!.isNotEmpty)
-                          Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(8.0),
-                              color: AdaptiveTheme.of(context).mode.isDark
-                                  ? const Color(0xff184741)
-                                  : const Color(0xffD2EAFA),
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Row(
-                                children: [
-                                  const Icon(Icons.info_outline_rounded),
-                                  const SizedBox(width: 10),
-                                  Flexible(
-                                      child: Text(
-                                          loanBloc.state.loan!.notification!)),
-                                ],
+                SizedBox(height: 20.h),
+                loanBloc.state.existLoan
+                    ? Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          if (loanBloc.state.loan!.notification!.isNotEmpty)
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8.0),
+                                color: AdaptiveTheme.of(context).mode.isDark
+                                    ? const Color(0xff184741)
+                                    : const Color(0xffD2EAFA),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Row(
+                                  children: [
+                                    const Icon(Icons.info_outline_rounded),
+                                    const SizedBox(width: 10),
+                                    Flexible(
+                                        child: Text(loanBloc
+                                            .state.loan!.notification!)),
+                                  ],
+                                ),
                               ),
                             ),
-                          ),
-                        if (loanBloc.state.loan!.payload!.inProcess!.isNotEmpty)
-                          loans('Prestamos en proceso:', [
-                            for (var item
-                                in loanBloc.state.loan!.payload!.inProcess!)
-                              CardLoanNew(
-                                  itemProcess: item,
-                                  color: const Color(0xffB3D4CF))
-                          ]),
-                        if (loanBloc.state.loan!.payload!.current!.isNotEmpty)
-                          loans('Prestamos vigentes:', [
-                            for (var item
-                                in loanBloc.state.loan!.payload!.current!)
-                              CardLoanNew(itemCurrent: item)
-                          ]),
-                        if (loanBloc.state.loan!.payload!.liquited!.isNotEmpty)
-                          loans('Prestamos Liquidados:', [
-                            for (var item
-                                in loanBloc.state.loan!.payload!.liquited!)
-                              CardLoanNew(itemCurrent: item)
-                          ]),
-                        if (loanBloc.state.loan!.error == 'true')
-                          Text(loanBloc.state.loan!.message!),
-                        const SizedBox(height: 70),
-                      ],
-                    )
-                  : Center(
-                      child: Image.asset(
-                      'assets/images/load.gif',
-                      fit: BoxFit.cover,
-                      height: 20,
-                    ))
-            ],
+                          if (loanBloc
+                              .state.loan!.payload!.inProcess!.isNotEmpty)
+                            loans('Prestamos en proceso:', [
+                              for (var item
+                                  in loanBloc.state.loan!.payload!.inProcess!)
+                                CardLoanNew(
+                                    itemProcess: item,
+                                    color: const Color(0xffB3D4CF))
+                            ]),
+                          if (loanBloc.state.loan!.payload!.current!.isNotEmpty)
+                            loans('Prestamos vigentes:', [
+                              for (var item
+                                  in loanBloc.state.loan!.payload!.current!)
+                                CardLoanNew(itemCurrent: item)
+                            ]),
+                          if (loanBloc
+                              .state.loan!.payload!.liquited!.isNotEmpty)
+                            loans('Prestamos Liquidados:', [
+                              for (var item
+                                  in loanBloc.state.loan!.payload!.liquited!)
+                                CardLoanNew(itemCurrent: item)
+                            ]),
+                          if (loanBloc.state.loan!.error == 'true')
+                            Text(loanBloc.state.loan!.message!),
+                          const SizedBox(height: 70),
+                        ],
+                      )
+                    : const Text("normal")
+                // : Center(
+                //     child: Image.asset(
+                //     'assets/images/load.gif',
+                //     fit: BoxFit.cover,
+                //     height: 20,
+                //   ))
+              ],
+            ),
           ),
         ),
+      ),
+      //ACA TINENE QUE ESTAR LA PRE - EVALUACION
+      floatingActionButton: FloatingActionButton(
+        foregroundColor: Colors.white,
+        onPressed: () {
+          showBarModalBottomSheet(
+            expand: false,
+            enableDrag: false,
+            isDismissible: true,
+            context: context,
+            builder: (context) => (const Text("Aca debe estar la calculadora")),
+          );
+        },
+        child: const Icon(Icons.add_circle),
       ),
     );
   }
